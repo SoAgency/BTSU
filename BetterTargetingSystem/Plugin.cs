8294df54733dc3f0d779f6bf4bcf00dab7dde661
SoAgency <so.agency@pm.me>
2026-09-17 13:31:58 +0200

    Update Plugin.cs

--- BetterTargetingSystem/Plugin.cs
+++ BetterTargetingSystem/Plugin.cs
@@ -114,10 +114,6 @@ public sealed unsafe class Plugin : IDalamudPlugin
         if (Client.IsLoggedIn == false || ObjectTable.LocalPlayer == null)
             return;
 
-        // Disable features in PvP
-        if (Client.IsPvP)
-            return;
-
         // Disable in GPose
         if (Client.IsGPosing)
             return;
